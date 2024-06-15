72ef3478b29ccb9f13d2e7c51652a4ac4dd153da
meskatabusayed <meskatabusayed@gmail.com>
2024-06-15 15:46:29 +0530

    update routes

--- src/app/routes/index.ts
+++ src/app/routes/index.ts
@@ -1,12 +1,8 @@
 import express from 'express';
 import { AuthRoutes } from '../modules/auth/auth.route';
 import { ServiceRoutes } from '../modules/service/service.route';
-
-// import { SlotRoutes, SlotRoutes2 } from '../modules/slot/slot.route';
-// import {
-//   BookingRoutes,
-//   BookingRoutes2,
-// } from '../modules/booking/booking.route';
+import { SlotRoutes, SlotRoutes2 } from '../modules/slot/slot.route';
+import { BookingRoutes, BookingRoutes2 } from '../modules/booking/booking.route';
 
 const router = express.Router();
 
@@ -19,22 +15,22 @@ const moduleRoutes = [
     path: '/services',
     route: ServiceRoutes,
   },
-//   {
-//     path: '/services/slots',
-//     route: SlotRoutes,
-//   },
-//   {
-//     path: '/slots/availability',
-//     route: SlotRoutes2,
-//   },
-//   {
-//     path: '/bookings',
-//     route: BookingRoutes,
-//   },
-//   {
-//     path: '/my-bookings',
-//     route: BookingRoutes2,
-//   },
+  {
+    path: '/services/slots',
+    route: SlotRoutes,
+  },
+  {
+    path: '/slots/availability',
+    route: SlotRoutes2,
+  },
+  {
+    path: '/bookings',
+    route: BookingRoutes,
+  },
+  {
+    path: '/my-bookings',
+    route: BookingRoutes2,
+  },
 ];
 
 moduleRoutes.forEach((item) => router.use(item.path, item.route));
